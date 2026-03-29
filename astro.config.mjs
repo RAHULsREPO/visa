7a02d36ed63574f169d0ea604753216473c7763e
vaibhavc13 <v599568@gmail.com>
2026-03-29 15:30:27 +0530

    added bug fix , added google adds

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,17 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://truegatevisa.com',
   vite: {
     plugins: [tailwindcss()],
     build: {
-      cssMinify: false
+      cssMinify: true
     }
   },
 
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
